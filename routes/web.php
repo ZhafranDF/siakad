0cd7f340f81f590916f0d347d9361f144710156f
ZhafranDF <zdf4753@gmail.com>
2023-09-19 19:20:12 +0700

    create user model

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages.blank-page', ['type_menu' => '']);
+    return view('auth.auth-login', ['type_menu' => '']);
 });
+
+/*
+Route::get('/auth-login', function () {
+    return view('auth.auth-login', ['type_menu' => '']);
+});
+
+Route::get('/auth-register', function () {
+    return view('auth.auth-register', ['type_menu' => '']);
+});
+
+Route::get('/auth-forgot-password', function () {
+    return view('auth.auth-forgot-password', ['type_menu' => '']);
+});
+
+Route::get('/auth-reset-password', function () {
+    return view('auth.auth-reset-password', ['type_menu' => '']);
+});
+*/
